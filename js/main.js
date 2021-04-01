714ac160a3cf035efae335619bd52222f8e35b04
Kobzarko <evgewi4@gmail.com>
2021-04-01 13:53:34 +0300

    add new slider with features

--- js/main.js
+++ js/main.js
@@ -394,47 +394,95 @@ window.addEventListener("DOMContentLoaded", function () {
 
   //*SLIDER
 
-  const arrowPrev = document.querySelector(".offer__slider-prev"),
-    arrowNext = document.querySelector(".offer__slider-next"),
+  const prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
     current = document.querySelector("#current"),
     slides = document.querySelectorAll(".offer__slide"),
-    total = document.querySelector("#total");
+    total = document.querySelector("#total"),
+    slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+    slidesField = document.querySelector(".offer__slider-inner"),
+    width = window.getComputedStyle(slidesWrapper).width;
 
   let slideIndex = 1;
+  let offset = 0;
 
-  showSlides(slideIndex);
+  // нумерация
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
 
-  function showSlides(counter) {
-    // если счетчик больше количества картинок то возвращаем к первой
-    if (counter > slides.length) {
+  // установить ширину этому блоку
+  slidesField.style.width = 100 * slides.length + "%";
+
+  // ограничить показ слайдов до одного
+  slidesWrapper.style.overflow = "hidden";
+
+  // разместить слайды горизонтально
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+
+  // каждому слайду устанавливаем одинаковую ширину
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  next.addEventListener("click", function () {
+    // если наш отступ равен ширине одного слайда умноженного на кол. слайдов - 1
+    // установить отступ в ноль
+    // долистали до самого конца и надо вернуться в самое начало
+    //  +width.slice(0, width.length - 2) получить число из ширины
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      //width => 500px
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    //смещаем наш блок
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    //контролируем slideIndex дошли до конца =1
+    if (slideIndex == slides.length) {
       slideIndex = 1;
+    } else {
+      slideIndex++;
     }
-    //если меньше то возвращаем к последней картинке
-    if (counter < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach((item) => (item.style.display = "none"));
-    slides[slideIndex - 1].style.display = "block";
-
+    // добавить ноль
     if (slides.length < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
-  }
-
-  function plusSlides(counter) {
-    showSlides((slideIndex += counter));
-  }
-
-  arrowPrev.addEventListener("click", function () {
-    plusSlides(-1);
   });
 
-  arrowNext.addEventListener("click", function () {
-    plusSlides(1);
+  prev.addEventListener("click", function () {
+    if (offset == 0) {
+      //width => 500px
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      //если не последний слайд листаем влево
+      offset -= +width.slice(0, width.length - 2);
+    }
+    //смещаем наш блок
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    //контролируем slideIndex дошли до конца =1
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+    // добавить ноль
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
   });
+
   //test
   // npm i json-server --save-dev
   // npm i
