5baae86fc75e026f9c60ea46d8b413a0c9579c12
Kobzarko <evgewi4@gmail.com>
2021-04-07 19:19:52 +0300

    added dots for the slider

--- js/main.js
+++ js/main.js
@@ -395,6 +395,7 @@ window.addEventListener("DOMContentLoaded", function () {
   //*SLIDER
 
   const prev = document.querySelector(".offer__slider-prev"),
+    slider = document.querySelector(".offer__slider"),
     next = document.querySelector(".offer__slider-next"),
     current = document.querySelector("#current"),
     slides = document.querySelectorAll(".offer__slide"),
@@ -403,7 +404,10 @@ window.addEventListener("DOMContentLoaded", function () {
     slidesField = document.querySelector(".offer__slider-inner"),
     width = window.getComputedStyle(slidesWrapper).width;
 
+  //Window.getComputedStyle()  возвращает объект, содержащий значения всех CSS-свойств элемента,
+  //полученных после применения всех активных таблиц стилей
   let slideIndex = 1;
+  // на сколько отступили вправо или влево
   let offset = 0;
 
   // нумерация
@@ -423,12 +427,60 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // разместить слайды горизонтально
   slidesField.style.display = "flex";
+  // сместить за полсекунды
   slidesField.style.transition = "0.5s all";
 
   // каждому слайду устанавливаем одинаковую ширину
   slides.forEach((slide) => {
     slide.style.width = width;
   });
+
+  // убираем абсолютное позиционирование у элементов внутри слайдера
+  // slider.style.postition = "relative";
+
+  // создать обертку для точек
+  const indicators = document.createElement("ol");
+  const dots = [];
+  indicators.classList.add("carousel-indicators");
+  indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+  `;
+  // добавляем в слайдер
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.setAttribute("data-slide-to", i + 1);
+    dot.style.cssText = `
+    box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+    `;
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
 
   next.addEventListener("click", function () {
     // если наш отступ равен ширине одного слайда умноженного на кол. слайдов - 1
@@ -437,11 +489,12 @@ window.addEventListener("DOMContentLoaded", function () {
     //  +width.slice(0, width.length - 2) получить число из ширины
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
       //width => 500px
+      console.log(width.length);
       offset = 0;
     } else {
       offset += +width.slice(0, width.length - 2);
     }
-    //смещаем наш блок
+    //смещаем наш блок по горизинтали
     slidesField.style.transform = `translateX(-${offset}px)`;
 
     //контролируем slideIndex дошли до конца =1
@@ -456,6 +509,9 @@ window.addEventListener("DOMContentLoaded", function () {
     } else {
       current.textContent = slideIndex;
     }
+    // назначаем прозрачность актуальной точке
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[slideIndex - 1].style.opacity = 1;
   });
 
   prev.addEventListener("click", function () {
@@ -481,6 +537,30 @@ window.addEventListener("DOMContentLoaded", function () {
     } else {
       current.textContent = slideIndex;
     }
+    // назначаем прозрачность актуальной точке
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[slideIndex - 1].style.opacity = 1;
+  });
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      //смещаем наш блок
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      // добавить ноль
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach((dot) => (dot.style.opacity = ".5"));
+      dots[slideIndex - 1].style.opacity = 1;
+    });
   });
 
   //test
