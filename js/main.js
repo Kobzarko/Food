a4221961b587b712fd8360f3db57c543961ffd07
Kobzarko <evgewi4@gmail.com>
2021-04-16 14:25:48 +0300

    added second part of the calculator

--- js/main.js
+++ js/main.js
@@ -591,18 +591,54 @@ window.addEventListener("DOMContentLoaded", function () {
   //* CALCULATING
 
   const result = document.querySelector(".calculating__result span");
-  let sex = "female",
-    height,
-    weight,
-    age,
+  let sex, height, weight, age, ratio;
+
+  if (localStorage.getItem("sex")) {
+    sex = localStorage.getItem("sex");
+  } else {
+    sex = "female";
+    localStorage.setItem("sex", sex);
+  }
+
+  if (localStorage.getItem("ratio")) {
+    ratio = localStorage.getItem("ratio");
+  } else {
     ratio = 1.375;
+    localStorage.setItem("ratio", ratio);
+  }
+
+  // изменяет класс активности элемента относительно данных в localstorage
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((elem) => {
+      elem.classList.remove(activeClass);
+      //если элемент содержит атрибут id который равен значению из localStorage
+      if (elem.getAttribute("id") === localStorage.getItem("sex")) {
+        // добавить класс активности элемента
+        elem.classList.add(activeClass);
+      }
+      //если элемент содержит атрибут data-ratio который равен значению из localStorage
+      if (elem.getAttribute("data-ratio") === localStorage.getItem("ratio")) {
+        // добавить класс активности элемента
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+  // вызов initLocalSettings
+  initLocalSettings("#gender div", "calculating__choose-item_active");
+  initLocalSettings(
+    ".calculating__choose_big div",
+    "calculating__choose-item_active"
+  );
+
   // изменяет данные каждый раз когда меняется значение в каком то поле
   function calcTotal() {
     if (!sex || !height || !weight || !age || !ratio) {
       result.textContent = "0";
       return;
     }
-
+    // рассчет каллорий
     if (sex === "female") {
       result.textContent = Math.round(
         (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
@@ -616,17 +652,23 @@ window.addEventListener("DOMContentLoaded", function () {
 
   calcTotal();
 
-  function getStaticInfo(parentSelector, activeClass) {
-    // получить все дивы элемента
-    const elements = document.querySelectorAll(`${parentSelector} div`);
+  function getStaticInfo(selector, activeClass) {
+    // получить все дивы элемента в элементе document.querySelectorAll(`${selector} div`);
+    const elements = document.querySelectorAll(selector);
 
     elements.forEach((elem) => {
       elem.addEventListener("click", (e) => {
         // если элемент содержит дата-ratio то взять с него значение для коффициента ratio
         if (e.target.getAttribute("data-ratio")) {
           ratio = +e.target.getAttribute("data-ratio");
+          //запись данных в localStorage
+          localStorage.setItem("ratio", ratio);
         } else {
           sex = e.target.getAttribute("id");
+          //запись данных в localStorage
+          if (sex) {
+            localStorage.setItem("sex", sex);
+          }
         }
 
         console.log(ratio, sex);
@@ -643,14 +685,23 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  getStaticInfo("#gender", "calculating__choose-item_active");
-  getStaticInfo(".calculating__choose_big", "calculating__choose-item_active");
+  getStaticInfo("#gender div", "calculating__choose-item_active");
+  getStaticInfo(
+    ".calculating__choose_big div",
+    "calculating__choose-item_active"
+  );
 
   // обрабатывает каждый отдельный инпут
   function getDynamicInfo(selector) {
     const input = document.querySelector(selector);
     // запись данных в определенную переменную
     input.addEventListener("input", () => {
+      //проверка валидности входящих данных
+      if (input.value.match(/\D/g)) {
+        input.style.border = "2px solid red";
+      } else {
+        input.style.border = "none";
+      }
       switch (input.getAttribute("id")) {
         case "height":
           height = +input.value;
